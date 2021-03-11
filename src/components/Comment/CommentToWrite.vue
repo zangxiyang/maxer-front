aa780fe04c991750eb6e6e6361a3f63612a2a68e
Seale <1801157108@qq.com>
2021-03-12 00:21:58 +0800

    [css] 修改element ui 多选框默认样式，使其符合主题配色

--- src/components/Comment/CommentToWrite.vue
+++ src/components/Comment/CommentToWrite.vue
@@ -13,7 +13,7 @@
             <el-col :lg="8">
               <div class="info-nickname">
                 <div class="title mb-5 maxer-require">昵称</div>
-                <input class="maxer-input small auto" name="name" />
+                <input class="maxer-input small auto" name="name"/>
               </div>
             </el-col>
             <el-col :lg="8">
@@ -29,6 +29,10 @@
               </div>
             </el-col>
           </el-row>
+          <div class="operation-container flex al-c">
+            <el-checkbox v-model="commentInfo.remember">记住我</el-checkbox>
+            <el-checkbox v-model="commentInfo.emailNotify">邮件通知</el-checkbox>
+          </div>
           <button class="maxer-border-button large mt-10">
             <icon-font name="iconziyuan" right="5px"/>
             <span>发布</span>
@@ -47,7 +51,7 @@
  * 作者: Seale
  * 时间: 2021/3/3
  * 版本: V1
-*/
+ */
 import {defineComponent} from 'vue';
 import IconFont from "@/components/IconFont.vue";
 
@@ -55,27 +59,81 @@ export default defineComponent({
   name: "CommentToWrite",
   components: {IconFont},
   data() {
-    return {}
+    return {
+      commentInfo: {
+        remember: false,    // 记住我
+        emailNotify: false, // 邮箱通知
+      }
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
-.maxer-comment-writer{
-  .header{
+.maxer-comment-writer {
+  .header {
     padding: 20px 30px;
     font-size: 1.3em;
   }
-  .maxer-comment-content{
+
+  .maxer-comment-content {
     padding: 0 30px;
     margin: 0 15px;
-    .content #comment{
+
+    .content #comment {
       min-width: 100%;
       width: 100%;
       min-height: 260px;
       resize: none;
       font-size: 17px;
       line-height: 17px;
+    }
+
+    .operation-container {
+      ::v-deep {
+        .el-checkbox__inner {
+          transition: .3s ease-out;
+          -webkit-transition: .3s ease-out;
+          &:hover {
+            border-color: #505050;
+          }
+
+          &:focus {
+            border-color: #505050;
+            outline: none;
+          }
+
+          &:active {
+            border-color: #505050;
+          }
+
+          &:visited {
+            border-color: #505050;
+            outline: none;
+          }
+        }
+
+        .el-checkbox {
+          &:hover .el-checkbox__inner {
+            border-color: #505050;
+            transition: .3s ease-out;
+            -webkit-transition: .3s ease-out;
+          }
+        }
+
+        .el-checkbox__input.is-checked .el-checkbox__inner {
+          background-color: #505050;
+          border-color: #505050;
+        }
+
+        .el-checkbox__input.is-focus .el-checkbox__inner {
+          border-color: #505050;
+        }
+
+        .el-checkbox__input.is-checked + .el-checkbox__label {
+          color: #111111;
+        }
+      }
     }
   }
 }
